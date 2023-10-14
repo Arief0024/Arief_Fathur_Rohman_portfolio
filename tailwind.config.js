36df46a2f36bb0cd4c1f169c6a7b0358a27e22d1
Arief0024 <rohmanarthur88.com>
2023-10-14 23:59:55 +0700

    update message

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: "#14b8a6",
+        primary: "#6d28d9",
         secondary: "#64748b",
         dark: "#0f172a",
       },
